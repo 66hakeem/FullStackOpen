67b9a3b02c246f6559389b4764be2e4186dd067d
Hakeem Matovu <66hakeem@gmail.com>
2025-01-21 16:10:56 +0300

    Course Information step 4

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,29 +1,70 @@
 
+const Header = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <Part part = {props.part1} exercise = {props.exercise1}/>
+      <Part part = {props.part2} exercise = {props.exercise2}/>
+      <Part part = {props.part3} exercise = {props.exercise3}/>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>Number of exercises {props.exercise1 + props.exercise2  + props.exercise3}</p>
+    </div>
+  )
+}
+
+const Part = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>{props.part} {props.exercise}</p>
+    </div>
+  )
+}
+
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course = {course} />
+      <Content part1 = {parts[0].name} exercise1 = {parts[0].exercises} 
+        part2 = {parts[1].name} exercise2 = {parts[1].exercises} 
+        part3 = {parts[2].name} exercise3 = {parts[2].exercises} 
+      />
+       <Total exercise1 = {parts[0]['exercises']} exercise2 = {parts[1].exercises} exercise3 = {parts[2].exercises} />
     </div>
   )
 
-}
+  }
 
 export default App
