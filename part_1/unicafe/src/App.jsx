8df735edd776f5bb87dd569031c27b7c385aff76
Hakeem Matovu <66hakeem@gmail.com>
2025-01-24 22:33:15 +0300

    unicafe step 5

--- part_1/unicafe/src/App.jsx
+++ part_1/unicafe/src/App.jsx
@@ -1,7 +1,10 @@
 import { useState } from 'react'
 
-const Statistics = (props) => {
-  if (props.all == 0) {
+const Statistics = ({good, neutral, bad}) => {
+
+  const all = good + neutral + bad
+
+  if (all == 0) {
     return (
       <div>
         <p>No feedback given</p>
@@ -10,12 +13,24 @@ const Statistics = (props) => {
   } 
   return (
     <div>
-      <p>Good: {props.good}</p>
-      <p>Neutral: {props.neutral}</p>
-      <p>Bad: {props.bad}</p>
-      <p>All: {props.all}</p>
-      <p>Average: {props.average}</p>
-      <p>Positive: {props.positive}</p>
+      <StatisticLine text={'Good'} value={good} />
+      <StatisticLine text={'Neutral'} value={neutral} />
+      <StatisticLine text={'Bad'} value={bad} />
+      <StatisticLine text={'All'} value={good + neutral + bad} />
+      <StatisticLine text={'Average'} value={(good - bad) / (good + neutral + bad)} />
+      <StatisticLine text={'Positive'} value={(good + neutral) / (good + neutral + bad) * 100 + '%'} />
+    </div>
+  )
+}
+
+const Button = (props) => (
+  <button onClick={props.onClick}>{props.text}</button>
+)
+
+const StatisticLine = (props) => {
+  return (
+    <div>
+      <p>{props.text}: {props.value}</p>
     </div>
   )
 }
@@ -25,15 +40,14 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [feedback, setFeedback] = useState(0)
 
   return (
     <div>
       <h1>Give Feedback</h1>
 
-      <button onClick={() => setGood(good + 1)}>Good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
-      <button onClick={() => setBad(bad + 1)}>Bad</button>
+      <Button onClick={() => setGood(good + 1)} text={'Good'} />
+      <Button onClick={() => setNeutral(neutral + 1)} text={'Neutral'} />
+      <Button onClick={() => setBad(bad + 1)} text={'Bad'} />
 
     <h1>Statistics</h1>
 
@@ -41,9 +55,6 @@ const App = () => {
           good = {good} 
           neutral = {neutral} 
           bad = {bad}
-          all = {good + neutral + bad}
-          average = {(good - bad) / (good + neutral + bad)}
-          positive = {(good / (good + neutral + bad)) * 100 + '%'}
     />
 
     </div>
